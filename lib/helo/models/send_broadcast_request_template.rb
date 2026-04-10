2678a206b2a86b1d0aa08997fff5435cb3851992
Jens Balvig <jens@balvig.com>
2026-04-10 14:46:57 +0900

    Update SDK

--- lib/helo/models/send_broadcast_request_template.rb
+++ lib/helo/models/send_broadcast_request_template.rb
@@ -4,7 +4,6 @@ module Helo
   class SendBroadcastRequestTemplate
     include Helo::Core::Model
 
-    api_attribute :id, :string, key: "id"
     api_attribute :subject, :string, key: "subject"
     api_attribute :html, :string, key: "html"
     api_attribute :text, :string, key: "text"
