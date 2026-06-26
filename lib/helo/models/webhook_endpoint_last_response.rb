a8c5639ab3e074d934ca20f65f4f881d6f1e980e
Jens Balvig <jens@balvig.com>
2026-06-26 11:49:58 +0900

    Update SDK

---
+++ lib/helo/models/webhook_endpoint_last_response.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Helo
+  class WebhookEndpointLastResponse
+    include Helo::Core::Model
+
+    api_attribute :status_code, :integer, key: "statusCode", nullable: true
+    api_attribute :error, :string, key: "error", nullable: true
+    api_attribute :at, :datetime, key: "at"
+  end
+end
--- lib/helo/models/webhook_endpoint_response.rb
+++ lib/helo/models/webhook_endpoint_response.rb
@@ -11,5 +11,6 @@ module Helo
     api_attribute :enabled, :boolean, key: "enabled"
     api_attribute :additional_headers, Helo::Core::ArrayType.new("Helo::WebhookHeader"), key: "additionalHeaders"
     api_attribute :events, key: "events"
+    api_attribute :last_response, :string, key: "lastResponse"
   end
 end
