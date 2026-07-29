d84ad4270ca9b62e2dcecf0a64815c33c0e185e3
Jimmy Choca <jimmy@helohq.com>
2026-07-29 15:34:38 -0400

    remove tracking/unsubscribe

--- lib/helo/models/dns_records_response.rb
+++ lib/helo/models/dns_records_response.rb
@@ -7,7 +7,5 @@ module Helo
     api_attribute :domain_key_active, Helo::Core::ModelType.new("Helo::DnsRecordResponse"), key: "domainKeyActive"
     api_attribute :domain_key_pending, Helo::Core::ModelType.new("Helo::DnsRecordResponse"), key: "domainKeyPending"
     api_attribute :return_path, Helo::Core::ArrayType.new("Helo::DnsRecordResponse"), key: "returnPath"
-    api_attribute :tracking, Helo::Core::ModelType.new("Helo::DnsRecordResponse"), key: "tracking"
-    api_attribute :unsubscribe, Helo::Core::ModelType.new("Helo::DnsRecordResponse"), key: "unsubscribe"
   end
 end
